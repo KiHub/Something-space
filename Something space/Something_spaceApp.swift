5ebee69c05009104ed1dc7f8e5aae052efd575cd
Kirill Ki <kirillkalapov@gmail.com>
2022-10-21 16:06:14 +0200

    Initial Commit

---
+++ Something space/Something_spaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Something_spaceApp.swift
+//  Something space
+//
+//  Created by  Mr.Ki on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Something_spaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
